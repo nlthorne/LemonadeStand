0e58e5a1afe4b921b58f0b37f476ef1e6b8c3663
nlthorne <nlthorne@wi.rr.com>
2016-09-27 14:46:07 -0500

    final fixes to UML diagram and Tests

---
+++ LemonadeStandTestProject/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LemonadeStand;
+
+namespace LemonadeStandTestProject
+{
+    [TestClass]
+    public class LemonadeStandTest
+
+    {
+        [TestMethod]
+        public void Setprice()
+        {
+            UserInput userinput = new UserInput();
+            //Arrange
+            double price = 1.50;
+            double result;
+            //Act
+            result = userinput.SetPrice();
+            //Assert
+            Assert.AreEqual(price, result);
+
+        }
+        [TestMethod]
+        public void TestPrice()
+        {
+            UserInput userinput = new UserInput();
+            //Arrange
+            double price = 500;
+            double result;
+            //Act
+            result = userinput.SetPrice();
+            //Assert
+            Assert.AreEqual(price, result);
+
+        }
+        [TestMethod]
+        public void TestSetPrice()
+        {
+            UserInput userinput = new UserInput();
+            //Arrange
+            double price = 0.00;
+            double result;
+            //Act
+            result = userinput.SetPrice();
+            //Assert
+            Assert.AreEqual(price, result);
+        }
+        [TestMethod]
+        public void NewTest()
+        {
+
+        }
+    }
+}
